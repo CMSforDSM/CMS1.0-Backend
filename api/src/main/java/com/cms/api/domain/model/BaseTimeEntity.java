d21a50a188a5e154861fc377b3e2c8fe5bb376d5
huewilliams <4879sinwoo@naver.com>
2020-06-25 11:09:05 +0900

    CMS-16 [ADD] base time entity

---
+++ api/src/main/java/com/cms/api/domain/model/BaseTimeEntity.java
@@ -0,0 +1,19 @@
+package com.cms.api.domain.model;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+}
