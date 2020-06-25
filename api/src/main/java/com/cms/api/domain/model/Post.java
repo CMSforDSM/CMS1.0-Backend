01ca45304916d9a443e76f62c3a4d2d17b540807
huewilliams <4879sinwoo@naver.com>
2020-06-25 11:09:51 +0900

    CMS-16 [ADD] post entity

---
+++ api/src/main/java/com/cms/api/domain/model/Post.java
@@ -0,0 +1,21 @@
+package com.cms.api.domain.model;
+
+import com.cms.api.domain.user.domain.User;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Post extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "writer")
+    private User writer;
+}
