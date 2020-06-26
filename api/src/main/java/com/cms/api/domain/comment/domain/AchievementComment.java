6ba5a3ce83ef7e4c70db8ea0f3917b15eb03e0bf
huewilliams <4879sinwoo@naver.com>
2020-06-26 15:59:44 +0900

    CMS-16 [ADD] child comment column

--- api/src/main/java/com/cms/api/domain/comment/domain/AchievementComment.java
+++ api/src/main/java/com/cms/api/domain/comment/domain/AchievementComment.java
@@ -2,13 +2,16 @@ package com.cms.api.domain.comment.domain;
 
 import com.cms.api.domain.achievement.domain.Achievement;
 import com.cms.api.domain.model.Comment;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
+@AllArgsConstructor
 @Getter
 @NoArgsConstructor
 @Entity
@@ -17,4 +20,8 @@ public class AchievementComment extends Comment {
     @ManyToOne
     @JoinColumn(name = "achievement_id")
     private Achievement achievementId;
+
+    @OneToOne
+    @JoinColumn(name = "child_comment")
+    private AchievementComment childComment;
 }
--- api/src/main/java/com/cms/api/domain/comment/domain/RecruitmentComment.java
+++ api/src/main/java/com/cms/api/domain/comment/domain/RecruitmentComment.java
@@ -2,13 +2,16 @@ package com.cms.api.domain.comment.domain;
 
 import com.cms.api.domain.model.Comment;
 import com.cms.api.domain.recruitment.domain.Recruitment;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
+@AllArgsConstructor
 @Getter
 @NoArgsConstructor
 @Entity
@@ -17,4 +20,8 @@ public class RecruitmentComment extends Comment {
     @ManyToOne
     @JoinColumn(name = "recruitment_id")
     private Recruitment recruitment;
+
+    @OneToOne
+    @JoinColumn(name = "child_comment")
+    private RecruitmentComment childComment;
 }
