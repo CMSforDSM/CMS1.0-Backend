140e4fc73dd7a6ef0db47bf5db01d9f15bc27fba
huewilliams <4879sinwoo@naver.com>
2020-07-07 15:37:43 +0900

    CMS-18 [ADD] new find method

--- api/src/main/java/com/cms/api/domain/user/dao/UserRepository.java
+++ api/src/main/java/com/cms/api/domain/user/dao/UserRepository.java
@@ -3,5 +3,9 @@ package com.cms.api.domain.user.dao;
 import com.cms.api.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByStudentNumber(String studentNo);
 }
