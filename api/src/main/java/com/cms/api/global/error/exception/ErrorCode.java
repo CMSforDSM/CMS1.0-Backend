044fe7d9a2a162572d7d71a84c80d95f3c5d57b4
huewilliams <4879sinwoo@naver.com>
2020-07-08 14:01:35 +0900

    CMS-18 [ADD] invalid token error code

--- api/src/main/java/com/cms/api/global/error/exception/ErrorCode.java
+++ api/src/main/java/com/cms/api/global/error/exception/ErrorCode.java
@@ -9,7 +9,10 @@ public enum ErrorCode {
     // Common
 
     // User
-    USER_NOT_FOUND(400, "U001", "User Not Found");
+    USER_NOT_FOUND(400, "U001", "User Not Found"),
+
+    // Auth
+    INVALID_TOKEN(401, "A001", "Invalid Token");
 
     private final int status;
     private final String code;
