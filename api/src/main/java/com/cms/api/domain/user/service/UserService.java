aa86f593639fb542724c4426cb82780fbfc6e565
huewilliams <4879sinwoo@naver.com>
2020-07-09 10:31:06 +0900

    CMS-19 [FIX] getMyInfo method

--- api/src/main/java/com/cms/api/domain/user/service/UserService.java
+++ api/src/main/java/com/cms/api/domain/user/service/UserService.java
@@ -7,6 +7,7 @@ import com.cms.api.domain.user.dao.UserRepository;
 import com.cms.api.domain.user.domain.Role;
 import com.cms.api.domain.user.domain.User;
 import com.cms.api.domain.user.dto.UserInfoResponseDto;
+import com.cms.api.domain.user.dto.UserInfoUpdateRequestDto;
 import com.cms.api.domain.user.dto.UserJoinRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -40,21 +41,19 @@ public class UserService {
     }
 
     public UserInfoResponseDto getMyInfo() {
-        User user = userRepository.findByStudentNumber(
-                SecurityContextHolder.getContext().getAuthentication().getName()
-        ).orElseThrow(UserNotFoundException::new);
-
-        String clubName = "";
-        if(user.getClub() != null) clubName = user.getClub().getClubName();
-        else clubName = null;
-
-        return UserInfoResponseDto.builder()
-                .id(user.getId())
-                .name(user.getName())
-                .studentNumber(user.getStudentNumber())
-                .introduce(user.getIntroduce())
-                .club(clubName)
-                .role(user.getRoleKey())
-                .build();
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByStudentNumber(username)
+                .map(user -> {
+                    String clubName = (user.getClub() != null) ? user.getClub().getClubName() : null;
+                    return UserInfoResponseDto.builder()
+                            .id(user.getId())
+                            .name(user.getName())
+                            .studentNumber(user.getStudentNumber())
+                            .introduce(user.getIntroduce())
+                            .club(clubName)
+                            .role(user.getRoleKey())
+                            .build();
+                })
+                .orElseThrow(UserNotFoundException::new);
     }
 }
