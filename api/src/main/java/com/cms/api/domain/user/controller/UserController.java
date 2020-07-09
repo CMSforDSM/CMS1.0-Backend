245ede92251ceab13b5c54cb00d46729bbc6d19c
huewilliams <4879sinwoo@naver.com>
2020-07-09 11:06:07 +0900

    CMS-19 [ADD] PATCH/users/me api

--- api/src/main/java/com/cms/api/domain/user/controller/UserController.java
+++ api/src/main/java/com/cms/api/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cms.api.domain.user.controller;
 
 import com.cms.api.domain.user.dto.UserInfoResponseDto;
+import com.cms.api.domain.user.dto.UserInfoUpdateRequestDto;
 import com.cms.api.domain.user.dto.UserJoinRequestDto;
 import com.cms.api.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.security.Principal;
 
 @RequiredArgsConstructor
@@ -26,5 +28,12 @@ public class UserController {
     @GetMapping("/me")
     public UserInfoResponseDto getMyInfo() {
         return userService.getMyInfo();
+    }
+
+    @PatchMapping("/me")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @Transactional
+    public void updateMyInfo(@RequestBody UserInfoUpdateRequestDto requestDto) {
+        userService.updateMyInfo(requestDto);
     }
 }
