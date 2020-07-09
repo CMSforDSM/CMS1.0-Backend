f890a47c1120e437ce4da3ff457a9255b740d7fa
huewilliams <4879sinwoo@naver.com>
2020-07-09 10:02:57 +0900

    CMS-19 [ADD] getMyInfo method

--- api/src/main/java/com/cms/api/domain/user/service/UserService.java
+++ api/src/main/java/com/cms/api/domain/user/service/UserService.java
@@ -1,18 +1,27 @@
 package com.cms.api.domain.user.service;
 
+import com.cms.api.domain.auth.exception.UserNotFoundException;
+import com.cms.api.domain.club.dao.ClubRepository;
+import com.cms.api.domain.club.domain.Club;
 import com.cms.api.domain.user.dao.UserRepository;
 import com.cms.api.domain.user.domain.Role;
 import com.cms.api.domain.user.domain.User;
+import com.cms.api.domain.user.dto.UserInfoResponseDto;
 import com.cms.api.domain.user.dto.UserJoinRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    private final ClubRepository clubRepository;
 
     private final PasswordEncoder passwordEncoder;
 
@@ -28,5 +37,23 @@ public class UserService {
                         .role(Role.valueOf(requestDto.getRole()))
                         .build()
         );
+    }
+
+    public UserInfoResponseDto getMyInfo() {
+        User user = userRepository.findByStudentNumber(
+                SecurityContextHolder.getContext().getAuthentication().getName()
+        ).orElseThrow(UserNotFoundException::new);
+
+        String clubName = "";
+        if(user.getClub() != null) clubName = user.getClub().getClubName();
+        else clubName = null;
+
+        return UserInfoResponseDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .studentNumber(user.getStudentNumber())
+                .club(clubName)
+                .role(user.getRoleKey())
+                .build();
     }
 }
