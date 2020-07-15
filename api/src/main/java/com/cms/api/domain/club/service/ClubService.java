1199388cdb960966e14e71444436e71c67be0d56
huewilliams <4879sinwoo@naver.com>
2020-07-15 12:08:01 +0900

    CMS-20 [ADD] getClubs service

--- api/src/main/java/com/cms/api/domain/club/service/ClubService.java
+++ api/src/main/java/com/cms/api/domain/club/service/ClubService.java
@@ -3,12 +3,16 @@ package com.cms.api.domain.club.service;
 import com.cms.api.domain.auth.exception.UserNotFoundException;
 import com.cms.api.domain.club.dao.ClubRepository;
 import com.cms.api.domain.club.domain.Club;
+import com.cms.api.domain.club.dto.ClubListResponseDto;
 import com.cms.api.domain.club.exception.ClubDuplicateException;
 import com.cms.api.domain.user.dao.UserRepository;
 import com.cms.api.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,6 +33,21 @@ public class ClubService {
                 .leader(user)
                 .build());
         return club.getClubName();
+    }
+
+    public List<ClubListResponseDto> getClubs() {
+        List<Club> clubs = clubRepository.findAll();
+
+        return clubs.stream()
+                .map(club -> {
+                    return ClubListResponseDto.builder()
+                            .club_name(club.getClubName())
+                            .introduce(club.getIntroduce())
+                            .leader_name(club.getLeader().getName())
+                            .leader_number(club.getLeader().getStudentNumber())
+                            .build();
+                })
+                .collect(Collectors.toList());
     }
 
 }
