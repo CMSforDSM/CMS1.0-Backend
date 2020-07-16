d4593b94b5b094788da025c64aeff0a9aab82fc9
huewilliams <4879sinwoo@naver.com>
2020-07-16 10:55:23 +0900

    CMS-18 [ADD] filterChainExceptionHandlerFilter

---
+++ api/src/main/java/com/cms/api/global/config/security/FilterChainExceptionHandlerFilter.java
@@ -0,0 +1,42 @@
+package com.cms.api.global.config.security;
+
+import com.cms.api.global.error.ErrorResponse;
+import com.cms.api.global.error.exception.ErrorCode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class FilterChainExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        try {
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            ErrorCode code = ErrorCode.AUTHENTICATION;
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setStatus(code.getStatus());
+
+            ErrorResponse errorResponse = new ErrorResponse(code.getStatus(), e.getMessage(), code.getCode());
+            response.getWriter().write(convertObjectToJson(errorResponse));
+        }
+    }
+
+    private String convertObjectToJson(Object object) throws JsonProcessingException {
+        if (object == null) {
+            return null;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(object);
+    }
+}
