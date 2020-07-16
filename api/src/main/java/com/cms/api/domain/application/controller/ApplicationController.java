fbc6c89eb8a95e2592b14a3c73b830632ca5c807
huewilliams <4879sinwoo@naver.com>
2020-07-16 15:18:53 +0900

    CMS-22 [ADD] GET/applications/{club_name}

--- api/src/main/java/com/cms/api/domain/application/controller/ApplicationController.java
+++ api/src/main/java/com/cms/api/domain/application/controller/ApplicationController.java
@@ -1,10 +1,13 @@
 package com.cms.api.domain.application.controller;
 
+import com.cms.api.domain.application.domain.Application;
+import com.cms.api.domain.application.dto.ApplicationInfoResponseDto;
 import com.cms.api.domain.application.service.ApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -18,6 +21,11 @@ public class ApplicationController {
     @ResponseStatus(value = HttpStatus.CREATED)
     public Long applyClub(@RequestBody Map<String, String> club) {
         return applicationService.createApplication(club.get("club_name"));
+    }
+
+    @GetMapping("/{club_name}")
+    public List<ApplicationInfoResponseDto> getApplications(@PathVariable String club_name) {
+        return applicationService.getApplications(club_name);
     }
 
 }
