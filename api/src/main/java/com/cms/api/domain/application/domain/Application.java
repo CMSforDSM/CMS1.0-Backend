601af89d0bf36c6018d67eb93bf38049865e29d0
huewilliams <4879sinwoo@naver.com>
2020-07-16 11:35:03 +0900

    CMS-22 [ADD] application entity

---
+++ api/src/main/java/com/cms/api/domain/application/domain/Application.java
@@ -0,0 +1,38 @@
+package com.cms.api.domain.application.domain;
+
+import com.cms.api.domain.club.domain.Club;
+import com.cms.api.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "applicant")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "club_name")
+    private Club club;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Builder
+    public Application(User user, Club club) {
+        this.user = user;
+        this.club = club;
+        this.status = Status.WAIT;
+    }
+}
