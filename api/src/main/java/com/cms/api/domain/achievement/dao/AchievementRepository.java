714224d522f46c2e80cd86d7f19741ce7504784f
huewilliams <4879sinwoo@naver.com>
2020-07-17 10:08:06 +0900

    CMS-25 [DELETE] deprecated entity & repository

--- api/src/main/java/com/cms/api/domain/achievement/dao/AchievementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cms.api.domain.achievement.dao;
-
-import com.cms.api.domain.achievement.domain.Achievement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AchievementRepository extends JpaRepository<Achievement, Long> {
-}
--- api/src/main/java/com/cms/api/domain/comment/dao/RecruitmentCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cms.api.domain.comment.dao;
-
-import com.cms.api.domain.recruitment.domain.Recruitment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecruitmentCommentRepository extends JpaRepository<Recruitment, Long> {
-}
