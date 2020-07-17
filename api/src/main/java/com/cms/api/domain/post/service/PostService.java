303d41b0a9f2c12300a0ed5f0c41df48b9ab2c14
huewilliams <4879sinwoo@naver.com>
2020-07-17 11:04:49 +0900

    CMS-25 [ADD] getPosts service

--- api/src/main/java/com/cms/api/domain/post/service/PostService.java
+++ api/src/main/java/com/cms/api/domain/post/service/PostService.java
@@ -4,15 +4,20 @@ import com.cms.api.domain.auth.exception.UserNotFoundException;
 import com.cms.api.domain.club.dao.ClubRepository;
 import com.cms.api.domain.club.domain.Club;
 import com.cms.api.domain.club.exception.ClubNotFoundException;
+import com.cms.api.domain.club.exception.NotClubMemberException;
 import com.cms.api.domain.post.dao.PostRepository;
 import com.cms.api.domain.post.domain.Post;
 import com.cms.api.domain.post.domain.PostType;
 import com.cms.api.domain.post.dto.PostCreateRequestDto;
+import com.cms.api.domain.post.dto.PostListResponseDto;
 import com.cms.api.domain.user.dao.UserRepository;
 import com.cms.api.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,9 +33,12 @@ public class PostService {
         String studentNo = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = userRepository.findByStudentNumber(studentNo).orElseThrow(UserNotFoundException::new);
         Club club = null;
+        PostType postType = PostType.valueOf(request.getType());
 
-        if(request.getClub_name() != null) {
+        if(request.getClub_name() != null || postType == PostType.ACHIEVEMENT || postType == PostType.RECRUITMENT) {
+            if(request.getClub_name() == null) throw new ClubNotFoundException();
             club = clubRepository.findById(request.getClub_name()).orElseThrow(ClubNotFoundException::new);
+            if(!club.checkMember(user)) throw new NotClubMemberException();
         }
 
         Post post = postRepository.save(Post.builder()
@@ -38,10 +46,33 @@ public class PostService {
                 .title(request.getTitle())
                 .writer(user)
                 .content(request.getContent())
-                .postType(PostType.valueOf(request.getType()))
+                .postType(postType)
                 .build());
 
         return post.getId();
+    }
+
+    public List<PostListResponseDto> getPosts(String type, String club_name) {
+
+        Club club = null;
+        List<Post> posts;
+
+        if(club_name != null) {
+            club = clubRepository.findById(club_name).orElseThrow(ClubNotFoundException::new);
+            posts = postRepository.findAllByPostTypeAndClub(PostType.valueOf(type), club);
+        } else {
+            posts = postRepository.findAllByPostType(PostType.valueOf(type));
+        }
+
+        return posts.stream()
+                .map(post -> {
+                    return PostListResponseDto.builder()
+                            .title(post.getTitle())
+                            .writer(post.getWriter().getStudentNumber() + "-" + post.getWriter().getName())
+                            .build();
+                })
+                .collect(Collectors.toList());
+
     }
 
 }
