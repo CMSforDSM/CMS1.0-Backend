60d0f456b3dbd8cf55b4c43be842256d6dda8074
huewilliams <4879sinwoo@naver.com>
2020-07-17 17:04:35 +0900

    CMS-28 [ADD] comment response dto

--- api/src/main/java/com/cms/api/domain/comment/dto/CommentResponseDto.java
+++ api/src/main/java/com/cms/api/domain/comment/dto/CommentResponseDto.java
@@ -5,14 +5,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentResponseDto {
 
+    private Long comment_id;
+    private Long post_id;
     private String writer;
-    private String title;
     private String content;
+    private List<Long> child_comments;
 
 }
