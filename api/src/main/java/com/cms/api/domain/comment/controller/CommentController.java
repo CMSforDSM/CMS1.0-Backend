9ac171ff37b3d6f3fec72a6f9af901cbc3e7dda0
huewilliams <4879sinwoo@naver.com>
2020-07-17 17:05:55 +0900

    CMS-28 [ADD] GET/comments API

--- api/src/main/java/com/cms/api/domain/comment/controller/CommentController.java
+++ api/src/main/java/com/cms/api/domain/comment/controller/CommentController.java
@@ -1,12 +1,13 @@
 package com.cms.api.domain.comment.controller;
 
+import com.cms.api.domain.comment.dto.CommentResponseDto;
 import com.cms.api.domain.comment.dto.CreateCommentRequestDto;
 import com.cms.api.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/comments")
@@ -16,8 +17,14 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
     public long createComment(@RequestBody CreateCommentRequestDto requestDto) {
         return commentService.createComment(requestDto);
+    }
+
+    @GetMapping
+    public List<CommentResponseDto> getComments(@RequestParam String post_id) {
+        return commentService.getComments(Long.parseLong(post_id));
     }
 
 }
