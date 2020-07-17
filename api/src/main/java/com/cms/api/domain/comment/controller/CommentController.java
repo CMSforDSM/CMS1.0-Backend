6a50f99d03474ce56dec33a24925c0487b59cb6e
huewilliams <4879sinwoo@naver.com>
2020-07-17 17:15:21 +0900

    CMS-28 [ADD] DELETE/comments/{comment_id} API

--- api/src/main/java/com/cms/api/domain/comment/controller/CommentController.java
+++ api/src/main/java/com/cms/api/domain/comment/controller/CommentController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
 
@@ -29,10 +30,19 @@ public class CommentController {
     }
 
     @PatchMapping("/{comment_id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @Transactional
     public void reviseComment(@PathVariable("comment_id") String commentId,
                               @RequestBody Map<String, String> content) {
 
         commentService.reviseComment(Long.parseLong(commentId), content.get("content"));
+    }
+
+    @DeleteMapping("/{comment_id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteComment(@PathVariable("comment_id") String commentId) {
+
+        commentService.deleteComment(Long.parseLong(commentId));
     }
 
 }
