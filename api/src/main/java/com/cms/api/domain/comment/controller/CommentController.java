685b4430111e6b6ad8e0ad26b315073d260e1efd
huewilliams <4879sinwoo@naver.com>
2020-07-17 17:12:02 +0900

    CMS-28 [ADD] PATCH/comments/{comment_id} API

--- api/src/main/java/com/cms/api/domain/comment/controller/CommentController.java
+++ api/src/main/java/com/cms/api/domain/comment/controller/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RequestMapping("/comments")
@@ -25,6 +26,13 @@ public class CommentController {
     @GetMapping
     public List<CommentResponseDto> getComments(@RequestParam String post_id) {
         return commentService.getComments(Long.parseLong(post_id));
+    }
+
+    @PatchMapping("/{comment_id}")
+    public void reviseComment(@PathVariable("comment_id") String commentId,
+                              @RequestBody Map<String, String> content) {
+
+        commentService.reviseComment(Long.parseLong(commentId), content.get("content"));
     }
 
 }
