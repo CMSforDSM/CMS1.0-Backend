34fd2624edd0102019c108a8aed0819389577c90
huewilliams <4879sinwoo@naver.com>
2020-07-18 23:46:29 +0900

    CMS-26 [ADD] GET/scouts API

--- api/src/main/java/com/cms/api/domain/scout/controller/ScoutController.java
+++ api/src/main/java/com/cms/api/domain/scout/controller/ScoutController.java
@@ -1,10 +1,12 @@
 package com.cms.api.domain.scout.controller;
 
+import com.cms.api.domain.scout.dto.ScoutResponseDto;
 import com.cms.api.domain.scout.service.ScoutService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -18,6 +20,11 @@ public class ScoutController {
     @ResponseStatus(value = HttpStatus.CREATED)
     public Long createScout(@RequestBody Map<String, String> target) {
         return scoutService.createScout(target.get("target"));
+    }
+
+    @GetMapping
+    public List<ScoutResponseDto> getScouts() {
+        return scoutService.getScouts();
     }
 
 }
