1b3f7cd0f2ee19ee07ad7294c372ffaa6427d6e6
huewilliams <4879sinwoo@naver.com>
2020-07-27 16:05:52 +0900

    Update deploy.sh

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl dalent | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl cms | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케아션 pid : $CURRENT_PID"
 
