af85915ef4fb98e06bbd0729291a665a7a47f168
huewilliams <4879sinwoo@naver.com>
2020-07-27 16:43:12 +0900

    [ADD] findByClubAndTarget method

--- api/src/main/java/com/cms/api/domain/scout/dao/ScoutRepository.java
+++ api/src/main/java/com/cms/api/domain/scout/dao/ScoutRepository.java
@@ -1,12 +1,15 @@
 package com.cms.api.domain.scout.dao;
 
+import com.cms.api.domain.club.domain.Club;
 import com.cms.api.domain.scout.domain.Scout;
 import com.cms.api.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ScoutRepository extends JpaRepository<Scout, Long> {
 
     List<Scout> findAllByTarget(User target);
+    Optional<Scout> findByClubAndTarget(Club club, User target);
 }
