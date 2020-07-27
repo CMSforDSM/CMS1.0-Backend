1da66803eccc825195d10407e734ee13f7c1f7e9
huewilliams <4879sinwoo@naver.com>
2020-07-27 16:26:31 +0900

    [ADD] findByUserAndClub method

--- api/src/main/java/com/cms/api/domain/application/dao/ApplicationRepository.java
+++ api/src/main/java/com/cms/api/domain/application/dao/ApplicationRepository.java
@@ -1,9 +1,12 @@
 package com.cms.api.domain.application.dao;
 
 import com.cms.api.domain.application.domain.Application;
+import com.cms.api.domain.club.domain.Club;
+import com.cms.api.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    Optional<Application> findByUserAndClub(User user, Club club);
 }
