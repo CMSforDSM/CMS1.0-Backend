4932fc6df787b62e1d6442bb8621a455dd6d0668
huewilliams <4879sinwoo@naver.com>
2020-07-28 19:49:10 +0900

    CMS-32 [ADD] getMembers method

--- api/src/main/java/com/cms/api/domain/club/service/MemberListServiceImpl.java
+++ api/src/main/java/com/cms/api/domain/club/service/MemberListServiceImpl.java
@@ -2,6 +2,7 @@ package com.cms.api.domain.club.service;
 
 import com.cms.api.domain.club.dao.ClubRepository;
 import com.cms.api.domain.club.domain.Club;
+import com.cms.api.domain.club.dto.ClubMemberResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,8 +24,6 @@ public class MemberListServiceImpl implements MemberListService {
     @Override
     @Transactional
     public void getMemberList(HttpServletResponse response) {
-        List<Club> clubs = clubRepository.findAll();
-
         XSSFWorkbook wb = new XSSFWorkbook();
 
         inputTitle(wb);
@@ -46,11 +46,25 @@ public class MemberListServiceImpl implements MemberListService {
         int rowIdx = 0;
         Row row = sheet.createRow(rowIdx++);
         Cell cell = null;
+
         String[] title = {"동아리", "학번", "이름", "출석여부"};
         int[] titleLen = {2, 1, 2, 3};
+
         for(int i = 0, j=0; j< title.length; i+=titleLen[j], j++) {
             cell = row.createCell(i);
             cell.setCellValue(title[j]);
         }
+    }
+
+    private List<ClubMemberResponseDto> getMembers() {
+        List<Club> clubs = clubRepository.findAll();
+        return clubs.stream()
+                .flatMap(club -> club.getClubMembers().stream())
+                .map(member -> ClubMemberResponseDto.builder()
+                        .club_name(member.split("-")[0])
+                        .student_no(member.split("-")[1])
+                        .name(member.split("-")[2])
+                        .build())
+                .collect(Collectors.toList());
     }
 }
